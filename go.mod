8ecc3b18343c5a96aee4d0dcb4aedbeab70b0bb1
ashish <ashirvadbhushan@gmail.com>
2022-06-18 11:22:28 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/asheeeshh/gosh
+
+go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/gorm v1.23.6
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type Handler struct {
+	db *gorm.DB
+}
+
+type Target struct {
+	Slug      string `json:"slug"`
+	TargetURL string `json:"target_url"`
+}
+
+func main() {
+	// Load environment variables from file.
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
+
+	// Connect to PlanetScale database using DSN environment variable.
+	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to PlanetScale: %v", err)
+	}
+	handler := NewHandler(db)
+
+	// Start an HTTP API server.
+	const addr = ":8080"
+	log.Printf("successfully connected to PlanetScale, starting HTTP server on %q", addr)
+	if err := http.ListenAndServe(addr, handler); err != nil {
+		log.Fatalf("failed to serve HTTP: %v", err)
+	}
+
+}
+
+func NewHandler(db *gorm.DB) http.Handler {
+	h := &Handler{db: db}
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleIndex).Methods(http.MethodGet)
+	r.HandleFunc("/seed", h.seedDatabase).Methods(http.MethodGet)
+	r.HandleFunc("/api/get/{slug}", h.getTarget).Methods(http.MethodGet)
+	r.HandleFunc("/api/put", h.putTarget).Methods(http.MethodPost)
+	r.HandleFunc("/{slug}", h.redirectToTarget).Methods(http.MethodGet)
+	return r
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, World!\n")
+}
+
+func (h *Handler) seedDatabase(w http.ResponseWriter, r *http.Request) {
+	if err := h.db.AutoMigrate(&Target{}); err != nil {
+		http.Error(w, "failed to migrate ghub table", http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w, "Migrations and Seeding of database complete\n")
+}
+
+func (h *Handler) redirectToTarget(w http.ResponseWriter, r *http.Request) {
+	var target Target
+	result := h.db.First(&target, h.db.Where("slug = ?", mux.Vars(r)["slug"]))
+	if result.Error != nil {
+		http.NotFound(w, r)
+		return
+	}
+	http.Redirect(w, r, target.TargetURL, http.StatusMovedPermanently)
+}
+
+func (h *Handler) checkForSlug(slug string) bool {
+	if err := h.db.Where("slug = ?", slug).First(&Target{}).Error; err != nil {
+		return false
+	}
+	return true
+}
+
+func validateSlug(slug string) bool {
+	return strings.TrimSpace(slug) != ""
+}
+
+func validateURL(targetURL string) bool {
+	_, err := url.ParseRequestURI(targetURL)
+	return err == nil
+}
+
+func (h *Handler) getTarget(w http.ResponseWriter, r *http.Request) {
+	var target Target
+	result := h.db.First(&target, h.db.Where("slug = ?", mux.Vars(r)["slug"]))
+	if result.Error != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	json.NewEncoder(w).Encode(target)
+}
+
+func (h *Handler) putTarget(w http.ResponseWriter, r *http.Request) {
+	var target Target
+	if err := json.NewDecoder(r.Body).Decode(&target); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if h.checkForSlug(target.Slug) || !validateSlug(target.Slug) {
+		if !validateURL(target.TargetURL) {
+			http.Error(w, "Target must be a valid URL!", http.StatusBadRequest)
+			return
+		}
+		http.Error(w, "Slug already exists, please try another one!", http.StatusBadRequest)
+		return
+	} else {
+		if err := h.db.Create(&target).Error; err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(target)
+}
